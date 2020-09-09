b78c39f68269704f102c2e1ce47c3089ddf0428e
doll6777 <doll6777@nate.com>
2020-09-09 22:03:34 +0900

    change naming

--- src/main/java/com/webhook/receiver/slack/webhook/WebhookSendController.java
+++ src/main/java/com/webhook/receiver/slack/webhook/WebhookSendController.java
@@ -9,18 +9,17 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/send")
-public class WebhookTestController {
+public class WebhookSendController {
 
     private final SlackNotifier slackNotifier;
     private final Gson gson = new Gson();
     
-    public WebhookTestController(SlackNotifier slackNotifier) {
+    public WebhookSendController(SlackNotifier slackNotifier) {
         this.slackNotifier = slackNotifier;
     }
     
     @RequestMapping(value = "/slack", method = RequestMethod.POST)
-    public ResponseEntity<Void> webhookTest(@RequestBody String payload) {
-        
+    public ResponseEntity<Void> sendWebhook(@RequestBody String payload) {
         // TODO: CheckerValue 추상클래스 역직렬화
         PinpointWebhookPayload pinpointWebhookPayload = gson.fromJson(payload, PinpointWebhookPayload.class);
         slackNotifier.send(pinpointWebhookPayload);
