df3076008387824b54e9ff92de4591a37c14a2dc
JongJin Bae <qowhdwlsdk@gmail.com>
2020-09-14 01:13:19 +0900

    [#7142] Add deserialize method in WebhookSendController

--- src/main/java/com/webhook/receiver/slack/webhook/WebhookSendController.java
+++ src/main/java/com/webhook/receiver/slack/webhook/WebhookSendController.java
@@ -1,28 +1,73 @@
 package com.webhook.receiver.slack.webhook;
 
-import com.google.gson.Gson;
-import com.webhook.receiver.slack.webhook.dto.PinpointWebhookPayload;
-import org.springframework.beans.factory.annotation.Value;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.webhook.receiver.slack.webhook.dto.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/send")
 public class WebhookSendController {
-
-    private final SlackNotifier slackNotifier;
-    private final Gson gson = new Gson();
     
-    public WebhookSendController(SlackNotifier slackNotifier) {
+    private final SlackNotifier slackNotifier;
+    private final ObjectMapper objectMapper;
+    
+    public WebhookSendController(SlackNotifier slackNotifier, ObjectMapper objectMapper) {
         this.slackNotifier = slackNotifier;
+        this.objectMapper = objectMapper;
     }
     
     @RequestMapping(value = "/slack", method = RequestMethod.POST)
     public ResponseEntity<Void> sendWebhook(@RequestBody String payload) {
         // TODO: CheckerValue 추상클래스 역직렬화
-        PinpointWebhookPayload pinpointWebhookPayload = gson.fromJson(payload, PinpointWebhookPayload.class);
-        slackNotifier.send(pinpointWebhookPayload);
+        WebhookPayload webhookPayload = null;
+        try {
+            webhookPayload = deserialize(payload);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        slackNotifier.send(webhookPayload);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+    
+    private WebhookPayload deserialize(String payload) throws Exception {
+        WebhookPayload webhookPayload = new WebhookPayload();
+        JsonNode jsonNode = objectMapper.readTree(payload);
+        
+        CheckerDetectedValue checkerDetectedValue = bindCheckerDetectedValue(jsonNode);
+        
+        webhookPayload.setPinpointUrl(jsonNode.get("pinpointUrl").asText());
+        webhookPayload.setBatchEnv(jsonNode.get("batchEnv").asText());
+        webhookPayload.setSequenceCount(jsonNode.get("sequenceCount").asInt());
+        webhookPayload.setApplicationId(jsonNode.get("applicationId").asText());
+        webhookPayload.setCheckerName(jsonNode.get("checkerName").asText());
+        webhookPayload.setThreshold(jsonNode.get("threshold").asInt());
+        webhookPayload.setNotes(jsonNode.get("notes").asText());
+        webhookPayload.setCheckerType(jsonNode.get("checkerType").asText());
+        webhookPayload.setUserGroupMemberPayload(objectMapper
+                .readValue(jsonNode.get("userGroupMemberPayload").toString(), UserGroupMemberPayload.class));
+        webhookPayload.setCheckerDetectedValue(checkerDetectedValue);
+        return webhookPayload;
+    }
+    
+    private CheckerDetectedValue bindCheckerDetectedValue(JsonNode jsonNode) throws Exception {
+        String checkerType = jsonNode.get("checkerType").asText();
+        if (checkerType.equals("LongValueAlarmChecker")) {
+            return objectMapper.readValue(jsonNode.get("checkerDetectedValue").toString(), LongValueAlarmCheckerDetectedValue.class);
+        } else if (checkerType.equals("LongValueAgentChecker")) {
+            return objectMapper.readValue(jsonNode.get("checkerDetectedValue").toString(), LongValueAgentCheckerDetectedValue.class);
+        } else if (checkerType.equals("BooleanValueAgentChecker")) {
+            return objectMapper.readValue(jsonNode.get("checkerDetectedValue").toString(), BooleanValueAgentCheckerDetectedValue.class);
+        } else if (checkerType.equals("DataSourceAlarmListValueAgentChecker")) {
+            return objectMapper.readValue(jsonNode.get("checkerDetectedValue").toString(), DataSourceAlarmListValueAgentCheckerDetectedValue.class);
+        } else {
+            throw new Exception();
+        }
     }
 }
