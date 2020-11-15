5b8b8df68d3ef42ebddb993baab46b6e64f4a3b7
doll6777 <doll6777@nate.com>
2020-11-15 21:36:23 +0900

    add todo

--- src/main/java/com/webhook/receiver/slack/webhook/vo/LongValueAlarmChecker.java
+++ src/main/java/com/webhook/receiver/slack/webhook/vo/LongValueAlarmChecker.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
 
 @JsonTypeName("LongValueAlarmChecker")
 public class LongValueAlarmChecker extends Checker {
+    // TODO : LongValueAlarmCheckerDetectedValue 로 바꿔야 하고, CheckerDetectedValue 타입의 Long Value 를 어떻게 넣을수 있는가?
     private LongValueAgentCheckerDetectedValue detectedValue;
     
     @Override
